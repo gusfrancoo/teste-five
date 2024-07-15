9e4c6e9e9c8babcc971e92045e073a4eb0f4185b
Gustavo Franco <gustafranco2543@gmail.com>
2024-07-15 11:42:26 -0300

    :sparkles: feat: adicionando rotas e controller pra manipulação de arquivos

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::post('/upload', [UploadFileController::class, 'upload']);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
